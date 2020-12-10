95c08fb89979404a9da5323511c8386886a37158
Borislav Valkov <52923477+B-V-Vulkov@users.noreply.github.com>
2020-12-10 21:33:37 +0200

    Add src

---
+++ ElementStatistic/ElementStatistic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElementStatistic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
